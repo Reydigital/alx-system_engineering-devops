738a37ae21cbf73bb6c49d5f18f1099e6563a12c
Reydigital <ysmarie@gmail.com>
2022-09-18 06:09:58 -0700

    commit

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# Installs puppet-lint v2.1.1.
+
+package { 'puppet-lint':
+
+  ensure   => '2.1.1',
+
+  provider => 'gem'
+
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# Kills the killmenow process.
+
+exec { 'killmenow':
+
+  command => 'pkill killmenow',
+
+  path    => '/usr/local/bin/:/usr/bin:/bin/',
+
+}
